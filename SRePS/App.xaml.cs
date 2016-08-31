b9e92de9ddee2f63e61cd435594c2786b0c3290b
Jake Pringle <Jake.B.Pringle@gmail.com>
2016-08-31 13:43:20 +1000

    test for corey

--- SRePS/App.xaml.cs
+++ SRePS/App.xaml.cs
@@ -16,7 +16,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace SRePS
-{
+{///hey Corey
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
