d9693d13e72c4c81c9a8dd27df0b325333f98cfa
Jess <jessicavilaysak@yahoo.com.au>
2016-09-06 13:21:32 +1000

    Can save a new sales order to 'salesorder.xml'.

--- SRePS/CreateSalesOrder.xaml.cs
+++ SRePS/CreateSalesOrder.xaml.cs
@@ -8,6 +8,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Documents;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using System.Text.RegularExpressions;
 
 // The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
 
@@ -15,11 +16,15 @@ namespace SRePS
 {
     public sealed partial class CreateSalesOrder : Page
     {
+        //List<SalesOrderInfo> LoadedSalesOrders = new List<SalesOrderInfo>();
         SalesOrder salesOrder = new SalesOrder();
+        SalesOrderInfo current_so = new SalesOrderInfo();
+        public double running_total = 0;
+
         public CreateSalesOrder()
         {
             this.InitializeComponent();
-            int id = salesOrder.NewSalesOrderId();
+            int id = salesOrder.GetCurrentId();
             textbox_id.Text = id.ToString();
         }
 
@@ -32,6 +37,9 @@ namespace SRePS
         {
             string item_name = textbox_item.Text;
             string quantity = textbox_quantity.Text;
+            
+            current_so.AddItem(item_name, Convert.ToDouble(quantity));
+            running_total += Convert.ToDouble(quantity);
             int item_quantity;
             if(int.TryParse(quantity, out item_quantity))
             {
@@ -43,6 +51,23 @@ namespace SRePS
                 textbox_error.Text = "Please enter a number into the quantity field!";
                 return;
             }
+            textbox_item.Text = "";
+            textbox_quantity.Text = "";
+            textbox_error.Text = "";
         }
+
+        private void button_save_Click(object sender, RoutedEventArgs e)
+        {
+            string user = textbox_user.Text;
+            string date = textbox_date.Text;
+
+            current_so.user = user;
+            current_so.date = date;
+            current_so.total = running_total;
+            current_so.id = 6.ToString();
+            salesOrder.loadSalesOrders().Add(current_so);
+            salesOrder.SaveSalesOrders();
+        }
+        
     }
 }
--- SRePS/SalesOrder.cs
+++ SRePS/SalesOrder.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
 
 namespace SRePS
 {
@@ -10,31 +12,113 @@ namespace SRePS
     {
         private List<SalesOrderInfo> salesOrderList = new List<SalesOrderInfo>();
 
-        private int NumberOfSalesOrders()
+        public void SaveSalesOrders()
         {
-            int count = 0;
-            foreach(SalesOrderInfo s in salesOrderList)
+            string output = "<?xml version=\"1.0\"?>\n<salesorderid>\n";
+            foreach (SalesOrderInfo so in salesOrderList)
             {
-                count++;
+                output += "<salesorderid salesorderid=\"" + so.id + "\">\n";
+                output += "<user>"+so.user+"</user>\n<date>"+so.date+"</date>\n";
+                int itemid = 0;
+                foreach(Items item in so.items)
+                {
+                    itemid++;
+                    output += "<item item=\"" + itemid.ToString() + "\">\n";
+                    output += "<item>" + item.item_name + "</item>\n";
+                    output += "<price>" + item.item_quantity + "</price>\n";
+                    output += "</item>\n";
+                }                                  
+                output += "<total>" + so.total + "</total>\n";
+                output += "</salesorderid>\n";
             }
-            return count;
+            output += "</salesorderid>";
+
+            StreamWriter writer;
+            FileStream fs = new FileStream(@"salesorder.xml", FileMode.Create, FileAccess.ReadWrite);
+            File.SetAttributes("salesorder.xml", FileAttributes.Normal);
+            writer = new StreamWriter(fs);
+            try
+            {
+                writer.Write(output);
+            }
+            finally
+            {
+                writer.Dispose();
+            }
         }
 
-        public int NewSalesOrderId()
+        public int GetCurrentId()
         {
-            int count = NumberOfSalesOrders();
-            return (count+1);
+            int id = 0;
+            foreach(SalesOrderInfo s in salesOrderList)
+            {
+                int s_id = Convert.ToInt32(s.id);
+                if (s_id > id)
+                    id = s_id;
+            }
+            return id+1;
         }
-        
+
+        public List<SalesOrderInfo> loadSalesOrders()
+        {
+            XElement rootElement = XElement.Load("JessTest.xml");
+            GetSalesOrders(0, rootElement);
+            return salesOrderList;
+        }
+
+        private void GetSalesOrders(int indentLevel, XElement element)
+        {
+            if (element.Attribute("salesorderid") != null)
+            {
+                SalesOrderInfo salesorder = new SalesOrderInfo();
+                salesorder.id = (string)element.Attribute("salesorderid");
+                salesorder.user = (string)element.Element("user");
+                salesorder.date = (string)element.Element("date");
+                salesorder.total = (double)element.Element("total");
+
+                if (element.Element("item") != null)
+                {
+                    GetItems(0, element, salesorder);
+                }
+
+                salesOrderList.Add(salesorder);
+            }
+
+            foreach (XElement childElement in element.Elements())
+            {
+                GetSalesOrders(indentLevel + 1, childElement);
+            }
+            return;
+        }
+
+        private void GetItems(int indentLevel, XElement element, SalesOrderInfo salesOrder)
+        {
+            if (element.Attribute("item") != null)
+            {
+                string itemName = (string)element.Element("item");
+                double itemPrice = (double)element.Element("price");
+
+                salesOrder.AddItem(itemName, itemPrice);
+            }
+
+            foreach (XElement childElement in element.Elements())
+            {
+                GetItems(indentLevel + 1, childElement, salesOrder);
+            }
+            return;
+        }
+
     }
 
     public class SalesOrderInfo
     {
-        public int id;
+        public string id;
+        public string user;
         public string date;
+        public double total;
         public List<Items> items = new List<Items>();
 
-        public void AddItem(string item, int quantity)
+        public void AddItem(string item, double quantity)
         {
             Items _item = new Items(item, quantity);
             items.Add(_item);
@@ -45,9 +129,9 @@ namespace SRePS
     public class Items
     {
         public string item_name;
-        public int item_quantity;
+        public double item_quantity;
 
-        public Items(string item, int quantity)
+        public Items(string item, double quantity)
         {
             item_name = item;
             item_quantity = quantity;
